45758080455141a45b9dafe18d9725bdc925746a
ierhe <yuningw05@gmail.com>
2025-10-23 12:24:04 -0400

    Create CartCounting.js

---
+++ CartCounting.js
@@ -0,0 +1,26 @@
+let prod = {
+    title: "Cooking With Fire"
+}
+
+const cart = [];
+function createCartObject(product){
+    const cartProduct = { ...product, quanitity: 1 };
+    return cartProduct
+}
+function checkCart(prod) {
+    if (!prod) {
+        return;
+    }
+    //create cart object here
+    const cartProduct = { ...prod, quanitity: 1 };
+    const found = cart.find((cartItem) => cartItem.title === cartProduct.title);
+    if (found) {
+        found.quantity += 1;
+    } else {
+        cart.push(cartProduct);
+    }
+}
+//function addToCart
+checkCart(prod);
+checkCart(prod);
+console.log(cart);
