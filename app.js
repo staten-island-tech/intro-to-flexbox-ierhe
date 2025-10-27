c165ff4ca0993533d9942dd0d4d324b4bea55f8a
ierhe <yuningw05@gmail.com>
2025-10-27 12:27:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,180 +1,179 @@
 const tapirs = [
-  {
-    name: "tapir and seedweed",
-    category: "water tapir",
-    price: 20,
-    img: "https://ideas.ted.com/wp-content/uploads/sites/3/2015/11/tapir_4_daniel-zupanc.jpg",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "grass tapir",
-    price: 15,
-    img: "https://th.bing.com/th/id/OIP.hvt0IDWKDsydPYZ0BLbTWQHaFA?w=286&h=193&c=7&r=0&o=7&pid=1.7&rm=3",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "land tapir",
-    price: 10,
-    img: "https://th.bing.com/th/id/OIP.UAeh2jRhxY2ouHOyOoRqhgHaEj?w=313&h=193&c=7&r=0&o=7&pid=1.7&rm=3",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "dirt tapir",
-    price: 20,
-    img: "https://imgs.mongabay.com/wp-content/uploads/sites/20/2019/01/14064748/animals_00884-762x512.jpg",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "land tapir",
-    price: 40,
-    img: "https://tse1.mm.bing.net/th/id/OIP.V9aeKEmWPjvZHdMFwy9ixQHaFj?rs=1&pid=ImgDetMain&o=7&rm=3",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "land tapir",
-    price: 30,
-    img: "https://tse2.mm.bing.net/th/id/OIP.MBxRql-4XGYCLZjZgUAVoQHaE8?rs=1&pid=ImgDetMain&o=7&rm=3",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "grass tapir",
-    price: 50,
-    img: "https://th.bing.com/th/id/R.422d88ed33afff59dd4335534d9e5d96?rik=CYBya9UyEbnjog&pid=ImgRaw&r=0",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "grass tapir",
-    price: 45,
-    img: "https://www.worldatlas.com/r/w1200/upload/07/81/69/shutterstock-534346312.jpg",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "baby tapir",
-    price: 35,
-    img: "https://animals.sandiegozoo.org/sites/default/files/inline-images/tapir_baby_animals.jpg",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "water tapir",
-    price: 5,
-    img: "https://th.bing.com/th/id/R.b5722c2630fbe139a48024dd01a8aaed?rik=b4TSrEFqVwpfJw&pid=ImgRaw&r=0",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "land tapir",
-    price: 10,
-    img: "https://th.bing.com/th/id/R.de23c02c473cff5749d5ba12da563451?rik=OeGFz8FrehMo9Q&pid=ImgRaw&r=0",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "land tapir",
-    price: 10,
-    img: "https://tse2.mm.bing.net/th/id/OIP.mIeYTHwhEXEcb-lRNj-hvAHaE8?rs=1&pid=ImgDetMain&o=7&rm=3",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "water tapir",
-    price: 25,
-    img: "https://tse4.mm.bing.net/th/id/OIP.bUJGKFNWxtm9hBLgxojQAQHaE6?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "water tapir",
-    price: 15,
-    img: "https://media.istockphoto.com/id/1146141198/photo/tapir-swimming-on-the-water-in-the-wildlife-sanctuary.jpg?s=612x612&w=0&k=20&c=G4WoNP-HNyHtIULB-MM_Fp_b0_XPY3nLl0RQ11Ta4dQ=",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "water tapir",
-    price: 5,
-    img: "https://thumbs.dreamstime.com/b/brazilian-tapir-water-brazilian-tapir-water-thailand-157360068.jpg",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "baby tapir",
-    price: 25,
-    img: "https://th.bing.com/th/id/OIP.huxROfDk1MHJMDWlCU_dPQHaEK?w=317&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "baby tapir",
-    price: 20,
-    img: "https://s.hdnux.com/photos/01/01/11/11/17076804/3/rawImage.jpg",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "baby tapir",
-    price: 30,
-    img: "https://tse3.mm.bing.net/th/id/OIP.j6zJ11E9fCJU7dftuiaypAHaE8?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "grass tapir",
-    price: 45,
-    img: "https://media.istockphoto.com/id/1464072172/photo/amazonian-tapir-standing-on-green-grass-at-the-zoo-with-blur-background.jpg?s=170667a&w=0&k=20&c=Q52zjyZt6xuArdicFcuTR-GA2BANYbLpYzz6PigWJQY=",
-    alt: "",
-    inStock: true,
-  },
-  {
-    name: "",
-    category: "grass tapir",
-    price: 50,
-    img: "https://i.pinimg.com/originals/16/e6/ab/16e6ab37df09c844fa2e8ff6d3872098.jpg",
-    alt: "",
-    inStock: true,
-  },
-];
+    {
+        name: "tapir and seedweed",
+        category: "water tapir",
+        price: 20,
+        img: "https://ideas.ted.com/wp-content/uploads/sites/3/2015/11/tapir_4_daniel-zupanc.jpg",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "grass tapir",
+        price: 15,
+        img: "https://th.bing.com/th/id/OIP.hvt0IDWKDsydPYZ0BLbTWQHaFA?w=286&h=193&c=7&r=0&o=7&pid=1.7&rm=3",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",        
+        category: "land tapir",
+        price: 10,
+        img: "https://th.bing.com/th/id/OIP.UAeh2jRhxY2ouHOyOoRqhgHaEj?w=313&h=193&c=7&r=0&o=7&pid=1.7&rm=3",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",        
+        category: "dirt tapir",
+        price: 20,
+        img: "https://imgs.mongabay.com/wp-content/uploads/sites/20/2019/01/14064748/animals_00884-762x512.jpg",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",        
+        category: "land tapir",
+        price: 40,
+        img: "https://tse1.mm.bing.net/th/id/OIP.V9aeKEmWPjvZHdMFwy9ixQHaFj?rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",        
+        category: "land tapir",
+        price: 30,
+        img: "https://tse2.mm.bing.net/th/id/OIP.MBxRql-4XGYCLZjZgUAVoQHaE8?rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "grass tapir",
+        price: 50,
+        img: "https://th.bing.com/th/id/R.422d88ed33afff59dd4335534d9e5d96?rik=CYBya9UyEbnjog&pid=ImgRaw&r=0",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "grass tapir",
+        price: 45,
+        img: "https://www.worldatlas.com/r/w1200/upload/07/81/69/shutterstock-534346312.jpg",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "baby tapir",
+        price: 35,
+        img: "https://animals.sandiegozoo.org/sites/default/files/inline-images/tapir_baby_animals.jpg",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "water tapir",
+        price: 5,
+        img: "https://th.bing.com/th/id/R.b5722c2630fbe139a48024dd01a8aaed?rik=b4TSrEFqVwpfJw&pid=ImgRaw&r=0",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "land tapir",
+        price: 10,
+        img: "https://th.bing.com/th/id/R.de23c02c473cff5749d5ba12da563451?rik=OeGFz8FrehMo9Q&pid=ImgRaw&r=0",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "land tapir",
+        price: 10,
+        img: "https://tse2.mm.bing.net/th/id/OIP.mIeYTHwhEXEcb-lRNj-hvAHaE8?rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "water tapir",
+        price: 25,
+        img: "https://tse4.mm.bing.net/th/id/OIP.bUJGKFNWxtm9hBLgxojQAQHaE6?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "water tapir",
+        price: 15,
+        img: "https://media.istockphoto.com/id/1146141198/photo/tapir-swimming-on-the-water-in-the-wildlife-sanctuary.jpg?s=612x612&w=0&k=20&c=G4WoNP-HNyHtIULB-MM_Fp_b0_XPY3nLl0RQ11Ta4dQ=",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "water tapir",
+        price: 5,
+        img: "https://thumbs.dreamstime.com/b/brazilian-tapir-water-brazilian-tapir-water-thailand-157360068.jpg",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "baby tapir",
+        price: 25,
+        img: "https://th.bing.com/th/id/OIP.huxROfDk1MHJMDWlCU_dPQHaEK?w=317&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "baby tapir",
+        price: 20,
+        img: "https://s.hdnux.com/photos/01/01/11/11/17076804/3/rawImage.jpg",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "baby tapir",
+        price: 30,
+        img: "https://tse3.mm.bing.net/th/id/OIP.j6zJ11E9fCJU7dftuiaypAHaE8?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "grass tapir",
+        price: 45,
+        img: "https://media.istockphoto.com/id/1464072172/photo/amazonian-tapir-standing-on-green-grass-at-the-zoo-with-blur-background.jpg?s=170667a&w=0&k=20&c=Q52zjyZt6xuArdicFcuTR-GA2BANYbLpYzz6PigWJQY=",
+        alt: "",
+        inStock: true,
+    },
+    {
+        name: "",
+        category: "grass tapir",
+        price: 50,
+        img: "https://i.pinimg.com/originals/16/e6/ab/16e6ab37df09c844fa2e8ff6d3872098.jpg",
+        alt: "",
+        inStock: true,
+    },
+]
 
 tapirs.forEach(inject);
 
-const AllBabyTapir = document.getElementById("AllTapirButton");
+const AllBabyTapir = document.getElementById("AllTapirButton")
 const ButtonBabyTapir = document.getElementById("BabyTapirButton");
 const ButtonGrassTapir = document.getElementById("GrassTapirButton");
 const ButtonWaterTapir = document.getElementById("WaterTapirButton");
 const ButtonLandTapir = document.getElementById("LandTapirButton");
 const container = document.querySelector(".container");
 
-function inject(tapirs) {
-  const container = document.querySelector(".container");
-  container.insertAdjacentHTML(
-    "beforeend",
-    `<div class="card">
+function inject(tapirs){
+    const container = document.querySelector(".container")
+    container.insertAdjacentHTML("beforeend",
+        `<div class="card">
         <img class="img" src="${tapirs.img}"/>
             <div class="Category">
                 <h1>${tapirs.category}</h1>
@@ -186,67 +185,62 @@ function inject(tapirs) {
                 <h2>Add to cart</h2>
             </button>
         </div>`
-  );
+    );
 }
 
-AllBabyTapir.addEventListener("click", function () {
-  container.innerHTML = "";
-  tapirs.forEach(inject);
+AllBabyTapir.addEventListener("click", function() {
+    container.innerHTML = "";
+    tapirs.forEach(inject);
+    addToCart();
+});
+
+ButtonBabyTapir.addEventListener("click", function() {
+    container.innerHTML = "";
+    const babyTapirs = tapirs.filter(tapir => tapir.category === "baby tapir");
+  babyTapirs.forEach(tapir => inject(tapir));
   addToCart();
 });
 
-ButtonBabyTapir.addEventListener("click", function () {
-  container.innerHTML = "";
-  const babyTapirs = tapirs.filter((tapir) => tapir.category === "baby tapir");
-  babyTapirs.forEach((tapir) => inject(tapir));
+ButtonGrassTapir.addEventListener("click", function() {
+    container.innerHTML = "";
+    const grassTapirs = tapirs.filter(tapir => tapir.category === "grass tapir");
+  grassTapirs.forEach(tapir => inject(tapir));
   addToCart();
 });
 
-ButtonGrassTapir.addEventListener("click", function () {
-  container.innerHTML = "";
-  const grassTapirs = tapirs.filter(
-    (tapir) => tapir.category === "grass tapir"
-  );
-  grassTapirs.forEach((tapir) => inject(tapir));
+ButtonWaterTapir.addEventListener("click", function() {
+    container.innerHTML = "";
+    const waterTapirs = tapirs.filter(tapir => tapir.category === "water tapir");
+  waterTapirs.forEach(tapir => inject(tapir));
   addToCart();
 });
 
-ButtonWaterTapir.addEventListener("click", function () {
-  container.innerHTML = "";
-  const waterTapirs = tapirs.filter(
-    (tapir) => tapir.category === "water tapir"
-  );
-  waterTapirs.forEach((tapir) => inject(tapir));
-  addToCart();
-});
-
-ButtonLandTapir.addEventListener("click", function () {
-  container.innerHTML = "";
-  const landTapirs = tapirs.filter((tapir) => tapir.category === "land tapir");
-  landTapirs.forEach((tapir) => inject(tapir));
+ButtonLandTapir.addEventListener("click", function() {
+    container.innerHTML = "";
+    const landTapirs = tapirs.filter(tapir => tapir.category === "land tapir");
+  landTapirs.forEach(tapir => inject(tapir));
   addToCart();
 });
 
 function addToCart() {
-  const Cart = document.querySelectorAll(".AddToCart");
-  const btnArray = Array.from(Cart);
-  btnArray.forEach((btn) =>
-    btn.dEventListener("click", function (event) {
-      const card = event.target.closest(".card");
-      const price = card.querySelector(".price").textContent;
-      const Catergory = card.querySelector(".Category").textContent;
-      const CartList = document.getElementById("cartList");
-      const TotalList = document.getElementById("TotalList");
-      const li = document.createElement("li");
-      const total = document.createElement("total");
-      total.textContent = TotalList.textContent += price;
-      TotalList.innerHTML = "";
-      li.textContent = `${Catergory} - ${price}`;
-      CartList.appendChild(li);
-      TotalList.appendChild(total);
-    })
-  );
+    const Cart = document.querySelectorAll(".AddToCart");
+    const btnArray = Array.from(Cart);
+    btnArray.forEach((btn) =>  
+        btn.addEventListener("click", function (event) {  
+            const card = event.target.closest(".card");
+            const price = (card.querySelector(".price").textContent);
+            const Catergory = (card.querySelector(".Category").textContent);
+            const CartList = document.getElementById("cartList");
+            const TotalList = document.getElementById("TotalList");
+            const li = document.createElement("li");
+            const total = document.createElement("total");
+            total.textContent = (TotalList.textContent += price);
+            TotalList.innerHTML = "";
+            li.textContent = `${Catergory} - ${price}`;
+            CartList.appendChild(li);
+            TotalList.appendChild(total);
+        }));
 }
-ad;
 
 addToCart();
+
